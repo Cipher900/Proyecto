e88c4beb75bad5da25740557ccd3bc2aedae1292
Feli <segurag1105@gmail.com>
2025-03-24 16:25:17 -0600

    más módulos y cambios
    deco y detector de erro

--- src/design/module_codificador.sv
+++ src/design/module_codificador.sv
@@ -1,12 +1,16 @@
-module codificador (
-    // Modulo codificador de una palabra de 4 bits. Primera parte del codigo de hamming.
-    input [3:0] in,      //Entrada de 4 bits [3,2,1,0]
-    output [6:0] out,     //Salida de 7 bits [7,6,5,4,3,2,1,0]
+module codificador ( // Modulo codificador de una palabra de 4 bits. Primera parte del codigo de hamming 3.
+    input logic [3:0] datos_in,      //Entrada de 4 bits [3,2,1,0]
+    output logic [6:0] datos_cod,     //Salida de 7 bits [7,6,5,4,3,2,1,0]
 );
-logic p1,p2,p3; // Bits de paridad
-assign p1 = in[0]^in[1]^in[3]; // p1 cubre la paridad de los bits 1,2,4
-assign p2 = in[0]^in[2]^in[3]; // p2 cubre la paridad de los bits 1,3,4
-assign p3 = in[1]^in[2]^in[3]; // p3 cubre la paridad de los bits 2,3,4
 
-assign out = {p1, p2, in[3], p3, in[2], in[1], in[0]}; // Salida de 7 bits. Palabra codificada.
+assign datos_cod[2] = datos_in[0]; // i0
+assign datos_cod[4] = datos_in[1]; // i1
+assign datos_cod[5] = datos_in[2]; // i2
+assign datos_cod[6] = datos_in[3]; // i3
+
+// bits de paridad
+assign datos_cod[0] = datos_in[0]^datos_in[1]^datos_in[3]; // c0 cubre la paridad de los bits 1,2,4 
+assign datos_cod[1] = datos_in[0]^datos_in[2]^datos_in[3]; // c1 cubre la paridad de los bits 1,3,4
+assign datos_cod[3] = datos_in[1]^datos_in[2]^datos_in[3]; // c2 cubre la paridad de los bits 2,3,4
+// Se trabaja con la función XOR para calcular los bits de paridad.
 endmodule
