cc7d2000c783e03b3d273bbde849eac60286ecc0
Cipher900 <gmorgan775@gmail.com>
2025-04-01 21:36:27 -0600

    Todo funciona

--- src/sim/module_corrector_error_tb.sv
+++ src/sim/module_corrector_error_tb.sv
@@ -2,16 +2,16 @@
 
 module module_corrector_error_tb;
         // Señales de prueba
-    reg [6:0] datos_rec; //Entrada: Datos recibidos
-    reg [2:0] sin; //Entrada: Síndrome de error
-    wire [6:0] datos_co; //Salida: Datos corregidos
+    reg [6:0] datos_recibidos; //Entrada: Datos recibidos
+    reg [2:0] sindrome; //Entrada: Síndrome de error
+    wire [6:0] data; //Salida: Datos corregidos
 
     // Instancia del módulo a probar 
 
     module_corrector_error dut (
-        .sindrome(sin),
-        .datos_recibidos(datos_rec),
-        .data(datos_co)
+        .sindrome(sindrome),
+        .datos_recibidos(datos_recibidos),
+        .data(data)
     );
 
     // Generación de estímulos
@@ -20,17 +20,25 @@ module module_corrector_error_tb;
         $display("Tiempo | Entrada | Sindrome | Salida");
         $display("-------|---------|----------|-------");
 
-       //Caso 1: Sin error
-       datos_rec=7'b000_0111; sin=3'b000;
-       $display("%t  |  %b  |   %b   |   %b", $time, datos_rec, sin, datos_co); #10;
+        //Caso 1: Sin error
+        //datos_recibidos =7'b000_0111; sindrome=3'b000;
+        //$display("%t  |  %b  |   %b   |   %b", $time, datos_recibidos, sindrome, data); #10;
 
-       //Caso 2: Error en el bit 1
-       datos_rec=7'b000_0110; sin=3'b001;
-       $display("%t  |  %b  |   %b   |   %b", $time, datos_rec, sin, datos_co); #10;
+        //Caso 2: Error en el bit 1
+        //datos_recibidos=7'b000_0110; sindrome=3'b001;
+        //$display("%t  |  %b  |   %b   |   %b", $time, datos_recibidos, sindrome, data); #20;
 
-       //Caso 3: Sin error
-         datos_rec=7'b111_1111; sin=3'b111;
-         $display("%t  |  %b  |   %b   |   %b", $time, datos_rec, sin, datos_co); #10;
+        //Caso 3: Sin error
+        //datos_recibidos=7'b111_1111; sindrome=3'b111;
+        //$display("%t  |  %b  |   %b   |   %b", $time, datos_recibidos, sindrome, data); #30;
+
+        $monitor("Sindrome = %b, Datos recibidos = %b, Data = %b", sindrome, datos_recibidos, data);
+
+        sindrome = 3'b000; datos_recibidos = 7'b1010101; #10; // Caso 1
+        sindrome = 3'b001; datos_recibidos = 7'b1110001; #10; // Caso 2
+        sindrome = 3'b010; datos_recibidos = 7'b1101010; #10; // Caso 3
+        sindrome = 3'b011; datos_recibidos = 7'b1001001; #10; // Caso 4
+        sindrome = 3'b111; datos_recibidos = 7'b0111110; #10; // Caso 5
 
     end
 
