238d8d42a5c1a07aeeeec5b3ccbd596833479e6c
Feli <segurag1105@gmail.com>
2025-04-03 07:30:12 -0600

    faltan los constraints
    todo medio funciona

--- src/sim/module_top_tb.sv
+++ src/sim/module_top_tb.sv
@@ -6,7 +6,7 @@ reg [3:0] entrada_tb;
 reg [6:0] palabra_tb;
 wire [6:0] siete_seg_tb;
 wire [3:0] led_o_tb;
-wire error_tb;
+wire [6:0] error_tb;
 
 // Instancia del módulo a probar
 module_top dut(
@@ -24,18 +24,18 @@ initial begin
     palabra_tb = 7'b0000000;
     $monitor("Tiempo: %0t | Entrada: %b | Palabra: %b | Siete Segmentos: %b | LED: %b | Error: %b", $time, entrada_tb, palabra_tb, siete_seg_tb, led_o_tb, error_tb);
     // Aplicar estímulos al módulo DUT
-    #10 entrada_tb = 4'b0001; palabra_tb = 7'b1111110; // Test case 1
-    #10 entrada_tb = 4'b0010; palabra_tb = 7'b0110000; // Test case 2
-    #10 entrada_tb = 4'b0011; palabra_tb = 7'b1101101; // Test case 3
-    #10 entrada_tb = 4'b0100; palabra_tb = 7'b1111001; // Test case 4
-    #10 entrada_tb = 4'b0101; palabra_tb = 7'b0110011; // Test case 5
-    #10 entrada_tb = 4'b0110; palabra_tb = 7'b1011011; // Test case 6
-    #10 entrada_tb = 4'b0111; palabra_tb = 7'b1011111; // Test case 7
-    #10 entrada_tb = 4'b1000; palabra_tb = 7'b1110000; // Test case 8
-    #10 entrada_tb = 4'b1001; palabra_tb = 7'b1111111; // Test case 9
+    #50 entrada_tb = 4'b0001; palabra_tb = 7'b0000111; // Test case 1
+    #50 entrada_tb = 4'b0010; palabra_tb = 7'b0010001; // Test case 2
+    #50 entrada_tb = 4'b0011; palabra_tb = 7'b0011110; // Test case 3
+    #50 entrada_tb = 4'b0100; palabra_tb = 7'b0101010; // Test case 4
+    #50 entrada_tb = 4'b0101; palabra_tb = 7'b0101100; // Test case 5
+    #50 entrada_tb = 4'b0110; palabra_tb = 7'b1011011; // Test case 6
+    #50 entrada_tb = 4'b0111; palabra_tb = 7'b0110100; // Test case 7
+    #50 entrada_tb = 4'b1000; palabra_tb = 7'b0001011; // Test case 8
+    #50 entrada_tb = 4'b1001; palabra_tb = 7'b1111111; // Test case 9
     
     // Finalizar la simulación después de un tiempo determinado
-    #50 $finish;
+    #100 $finish;
 end
 initial begin
     $dumpfile("module_top_tb.vcd");
